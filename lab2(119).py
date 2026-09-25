da40f7aff9316e87691fd1bb33c02e8a9cf344be
Alevtyna <alevtynafrantsuz@gmail.com>
2026-09-25 21:35:39 +0300

    Create lab2(119).py

---
+++ lab2(119).py
@@ -0,0 +1,14 @@
+a = int(input())
+if a >= 90 and a <= 100:
+    print("your grade is A")
+elif a > 100:
+    print("this grade not exist")
+elif a >= 80 and a <= 89:
+    print("your grade is B")
+elif a >= 70 and a <= 79:
+    print("your grade is C")
+elif a >= 60 and a <= 69:
+    print("your grade is D")
+elif a < 60 and a >= 0:
+    print("your grade is F")
+    
